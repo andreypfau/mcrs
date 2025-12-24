9f1a9e1a0d4b04b199c22ade381d1a2fc4c8c205
andreypfau <andreypfau@ton.org>
2025-12-24 23:58:20 +0400

    Refactor sound system to integrate `bevy_asset` and reorganize `sound` module structure

--- crates/mcrs_minecraft/src/sound/mod.rs
+++ crates/mcrs_minecraft/src/sound/mod.rs
@@ -1,8 +1,10 @@
+use bevy_asset::{Asset, Handle, uuid_handle};
+use bevy_reflect::TypePath;
 use mcrs_protocol::Ident;
 use mcrs_registry::{Holder, RegistryEntry};
 use serde::{Deserialize, Serialize};
 
-#[derive(Clone, Debug, Serialize, Deserialize)]
+#[derive(Clone, Debug, Serialize, Deserialize, Asset, TypePath)]
 pub struct SoundEvent {
     sound_id: Ident<String>,
     range: f32,
@@ -17,3 +19,5 @@ pub struct Music {
     max_delay: i32,
     replace_current_music: bool,
 }
+
+pub const ITEM_BREAK: Handle<SoundEvent> = uuid_handle!("019b30d8-a349-78b7-b7f5-d94ad743dd6d");
